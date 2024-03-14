b9a5b4aae34a4c093240787e93f136cd91828e59
Flavius Stefan <flaviusstefanungureanu@gmail.com>
2024-03-14 19:47:16 +0200

    UI - display categories multi-select dropdown and capture categories

--- src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
+++ src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
@@ -1,28 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AddBlogPost } from '../models/add-blog-post.model';
 import { BlogPostService } from '../services/blog-post.service';
 import { Router } from '@angular/router';
+import { CategoryService } from '../../category/services/category.service';
+import { Observable } from 'rxjs';
+import { Category } from '../../category/models/category.model';
 
 @Component({
   selector: 'app-add-blogpost',
   templateUrl: './add-blogpost.component.html',
   styleUrls: ['./add-blogpost.component.css']
 })
-export class AddBlogpostComponent {
+export class AddBlogpostComponent implements OnInit {
 model: AddBlogPost;
+categories$?: Observable<Category[]>;
 
-  constructor(private blogPostService: BlogPostService, private router: Router){
-    this.model = {
-    title: '',
-    shortDescription: '',
-    urlHandle: '',
-    content: '',
-    featuredImageUrl: '',
-    author: '',
-    isVisible: true,
-    publishedDate: new Date()
-
+  constructor(private blogPostService: BlogPostService,
+    private categoryService: CategoryService,
+    private router: Router ){
+      this.model = {
+      title: '',
+      shortDescription: '',
+      urlHandle: '',
+      content: '',
+      featuredImageUrl: '',
+      author: '',
+      isVisible: true,
+      publishedDate: new Date()
     }
+  }
+
+  ngOnInit(): void {
+    this.categories$ = this.categoryService.getAllCategories();
   }
 
   onFormSubmit(): void {
