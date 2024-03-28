94a33d953924886e2add3a1d833e189ad7e381f2
Flavius Stefan <flaviusstefanungureanu@gmail.com>
2024-03-29 01:50:21 +0200

    Created "home" component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,23 +6,28 @@ import { EditCategoryComponent } from './features/category/edit-category/edit-ca
 import { BlogpostListComponent } from './features/blog-post/blogpost-list/blogpost-list.component';
 import { AddBlogpostComponent } from './features/blog-post/add-blogpost/add-blogpost.component';
 import { EditBlogpostComponent } from './features/blog-post/edit-blogpost/edit-blogpost.component';
+import { HomeComponent } from './features/public/home/home.component';
 
 const routes: Routes = [
   {
+    path:'',
+    component: HomeComponent
+  },
+  {
     path: 'admin/categories',
-    component: CategoryListComponent,
+    component: CategoryListComponent
   },
   {
     path: 'admin/categories/add',
-    component: AddCategoryComponent,
+    component: AddCategoryComponent
   },
   {
     path: 'admin/categories/:id',
-    component: EditCategoryComponent,
+    component: EditCategoryComponent
   },
   {
     path: 'admin/blogposts',
-    component: BlogpostListComponent,
+    component: BlogpostListComponent
   },
   {
     path: 'admin/blogposts/add',
