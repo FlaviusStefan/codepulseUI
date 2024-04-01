95a795c12ef885faa9873b84dec662f819a1b384
Flavius Stefan <flaviusstefanungureanu@gmail.com>
2024-04-01 12:55:28 +0300

    UI - created BlogDetails component

---
+++ src/app/features/public/blog-details/blog-details.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-blog-details',
+  templateUrl: './blog-details.component.html',
+  styleUrls: ['./blog-details.component.css']
+})
+export class BlogDetailsComponent implements OnInit{
+
+  url: string | null = null;
+  constructor(private route: ActivatedRoute){
+
+  }
+  ngOnInit(): void {
+    this.route.paramMap
+      .subscribe({
+        next: (params) => {
+          this.url = params.get('url');
+        }
+      });
+    
+    // Fetch blohg details by url
+  }
+
+
+
+}
